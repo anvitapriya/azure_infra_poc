18889d0b3a440246df3519939e9399a30b74d3bd
anvitapriya <47037279+anvitapriya@users.noreply.github.com>
2019-04-15 16:01:01 +0530

    Update main.tf
    OK

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ resource "azurerm_virtual_machine" "TenantDocker" {
   name                  = "${var.vm_name}"
   location              = "${var.location}"
   resource_group_name   = "${var.resource_group_name}"
-  network_interface_ids = ["${azurerm_network_interface.TenantDocker.id}"]
+  network_interface_ids = ["${azurerm_network_interface.TenantDockerNIC.id}"]
   vm_size               = "Standard B2s"
 
   # Uncomment this line to delete the OS disk automatically when deleting the VM
